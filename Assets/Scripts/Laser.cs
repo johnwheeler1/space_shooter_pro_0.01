86b56e6c2930628091b466255468c4f34ac21984
tmoneygames <tmoneygames2019@gmail.com>
2021-12-12 19:53:49 -0800

    Created Laser and Laser.cs, Enemy and Enemy.cs, added extra Asset Packs folder with assets. "JW" 12/12/2021

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // Variables
+    [SerializeField] private float _speed = 8f;
+    
+
+    
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        if (transform.position.y >= 8)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,9 +7,12 @@ public class Player : MonoBehaviour
     // Variables
     [SerializeField] private float _speed = 3.5f;
 
+    [SerializeField] private GameObject _laserPrefab;
+    [SerializeField] private float _fireRate = 0.15f;
+    private float _canFire = -1f;
+
     void Start()
     {
-        // take current position = new position(0, 0, 0)
         transform.position = new Vector3(0, 0, 0);
     }
 
@@ -17,6 +20,11 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
+        
+        if(Input.GetKeyDown(KeyCode.Space) && Time.time >= _canFire)
+        {
+            FireLaser();
+        }
     }
 
     void CalculateMovement()
@@ -38,5 +46,11 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
+    }
+
+    void FireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
     }
 }
